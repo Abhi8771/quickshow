7ee31ed2803776e45fe1a044099b9b9e1810bce7
Abhi8771 <abhi8771@gmail.com>
2025-07-24 20:45:40 +0530

    admin done

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/title';
+import dateFormat from '../../lib/dateFormat';
+
 
 const ListShows = () => {
 
@@ -14,7 +16,7 @@ const ListShows = () => {
     try {
       setShows([{
         movie: dummyShowsData[0],
-        showDataTime: "2025-06-30T02:30:00.000Z",
+        showDateTime: "2025-06-30T02:30:00.000Z",
         showPrice: 59,
         occupiedSeats:{
           A1: "user_1",
@@ -30,7 +32,7 @@ const ListShows = () => {
   }
     useEffect(() => {
       getAllShows();
-    })
+    },[])
 
   return !loading ? (
     <>
@@ -45,6 +47,16 @@ const ListShows = () => {
           <th className='p-2 font-medium '>Earnings</th>
         </tr>
         </thead>
+        <tbody className='text-sm font-light'>
+          {shows.map((show, index) => (
+            <tr key={index} className='border-b border-primary/10 bg-primary/5 even:bg-primary/10'>
+              <td className='p-2 min-w-45 pl-5'>{show.movie.title}</td>
+              <td className='p-2'>{dateFormat(show.showDateTime)}</td>
+              <td className='p-2'>{Object.keys(show.occupiedSeats).length}</td>
+              <td className='p-2'>{currency}{Object.keys(show.occupiedSeats).length * show.showPrice}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
     </>
