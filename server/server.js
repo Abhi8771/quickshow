8121573d8b6c7d72661c2390eac6dd14b80da057
Abhi8771 <abhi8771@gmail.com>
2025-07-25 09:56:57 +0530

    inngest

--- server/server.js
+++ server/server.js
@@ -25,5 +25,5 @@ app.get('/', (req, res) => {
 app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost: ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
